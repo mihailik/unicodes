60b01e8b3166a8b5260407cf610640b42c285a42
Oleg Mihailik <mihailik@gmail.com>
2021-01-20 23:07:18 +0000

    Basic stuff.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/// <reference types="codemirror" />
+var editorHost = document.createElement('div');
+editorHost.className = 'editorHost';
+document.body.appendChild(editorHost);
+
+var sideHost = document.createElement('div');
+sideHost.className = 'sideHost';
+document.body.appendChild(sideHost);
+
+var editor = CodeMirror(
+  editorHost,
+  {
+    // yes
+  });
